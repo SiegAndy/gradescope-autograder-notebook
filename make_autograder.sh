21be8b82b76d9e868a402abb4f5085f11a2ca103
Annie <zc470618625@gmail.com>
2024-09-28 22:41:24 -0400

    [Fix] add stopwords to autograder

--- make_autograder.sh
+++ make_autograder.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-zip -r autograder.zip setup.sh run_autograder run_tests.py solution.py requirements.txt tests/*.py data/P1-train.gz
+zip -r autograder.zip setup.sh run_autograder run_tests.py solution.py requirements.txt tests/*.py data/P1-train.gz data/stopwords.txt
