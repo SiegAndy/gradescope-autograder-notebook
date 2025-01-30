d9dc2f1b2677c71235350dc7bc03c92f0e18e1ae
T'aldarim <86433624+SiegAndy@users.noreply.github.com>
2025-01-30 14:42:45 -0500

    [Fix] Update p1 train zipper

--- tests/PA1/make_p1_train.sh
+++ tests/PA1/make_p1_train.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-echo "creating P1-train.zip"
 rm -f P1-train.zip
 
 zip -r P1-train.zip \
     train/msmarco.qrels train/msmarco.queries \
-    train/*.trecrun
-
-echo "creating P1-train-output.zip"
-rm -f P1-train-output.zip
-
-zip -r P1-train-output.zip \
+    train/*.trecrun \
     output/*.expeval
